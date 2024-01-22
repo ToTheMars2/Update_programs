dc7e6dae9d8367887fd868658de701c3c8cb505f
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2024-01-22 15:50:38 +0200

    Create update_go.sh

---
+++ update_go.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Версія Go, яку ви хочете встановити
+GO_VERSION=${1:-"1.19.5"}
+
+# Видалення попередньої версії Go
+sudo rm -rf /usr/local/go
+
+# Завантаження та встановлення нової версії Go
+wget https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
+rm go$GO_VERSION.linux-amd64.tar.gz
+read -p "Add path to .bashrc? (y/n): " answer
+if [ "$answer" == "y" ]; then
+    echo -e "export GOROOT=/usr/local/go\nexport GOPATH=$HOME/go\nexport PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> ~/.bashrc
+    source ~/.bashrc
+    echo "Paths added to .bashrc and sourced."
+else
+    echo "No changes made to .bashrc."
+fi
+# Вивести версію Go
+go version
