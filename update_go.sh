4169b0fa9993c4f1c3c2a49c23d8895b5187b44f
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2024-08-20 18:26:17 +0300

    Update update_go.sh

--- update_go.sh
+++ update_go.sh
@@ -10,13 +10,14 @@ sudo rm -rf /usr/local/go
 wget https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
 rm go$GO_VERSION.linux-amd64.tar.gz
+
+
+sed -i '/export GOROOT=\/usr\/local\/go/d' ~/.bashrc
+sed -i '/export GOPATH=\$HOME\/go/d' ~/.bashrc
+sed -i '/export PATH=\$GOPATH\/bin:\$GOROOT\/bin:\$PATH/d' ~/.bashrc
 read -p "Add path to .bashrc? (y/n): " answer
-if [ "$answer" == "y" ]; then
-    echo -e 'export GOROOT=/usr/local/go\nexport GOPATH=$HOME/go\nexport PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
-    echo "you need enter   source .bashrc "
-else
-    echo ""
-fi
+echo -e 'export GOROOT=/usr/local/go\nexport GOPATH=$HOME/go\nexport PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+echo "you need enter   source .bashrc "
 # Вивести версію Go
 sleep 1
 source ~/.bashrc
